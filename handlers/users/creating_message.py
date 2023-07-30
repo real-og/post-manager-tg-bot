bfe5072c0a877db19874eec4a7a5757d0f1e0262
Evgeniy Grakovich <grakovich-e@yandex.by>
2023-07-30 03:42:12 +0300

    interface with buttons

--- handlers/users/creating_message.py
+++ handlers/users/creating_message.py
@@ -29,7 +29,7 @@ async def send_channels(callback: types.CallbackQuery, state: FSMContext):
         await callback.message.answer(texts.success_posted)
         await State.entering_code.set()
     elif callback.data == 'buttons':
-        await callback.message.answer(texts.instruction_for_buttons)
+        await callback.message.answer(texts.instruction_for_buttons, reply_markup=kb.abort_kb)
         await State.adding_buttons.set()
     elif callback.data == 'schedule':
         await callback.message.answer(texts.ask_for_day_to_send, reply_markup=kb.choose_day_kb)
@@ -42,32 +42,29 @@ async def send_channels(callback: types.CallbackQuery, state: FSMContext):
 
 @dp.message_handler(state=State.adding_buttons)
 async def send_welcome(message: types.Message, state: FSMContext):
+    if message.text == texts.abort:
+        data = await state.get_data()
+        message_id = data.get('message_id')
+        kb_text = data.get('inline_kb_text')
+        custom_kb = kb.create_user_keyboard(logic.convert_input_to_buttons(kb_text))
+        await bot.copy_message(message.from_id, message.from_id, message_id, reply_markup=custom_kb)
+        await message.answer(texts.confirm_message, reply_markup=kb.message_menu_kb)
+        await State.confirmation_message.set()
+        return
+
+
     await state.update_data(inline_kb_text=message.text)
 
     data = await state.get_data()
     kb_text = data.get('inline_kb_text')
     message_id = data.get('message_id')
-    custom_kb = kb.create_user_keyboard(logic.convert_input_to_buttons(kb_text))
+    try:
+        custom_kb = kb.create_user_keyboard(logic.convert_input_to_buttons(kb_text))
+    except:
+        await message.answer(texts.error_buttons, reply_markup=kb.abort_kb)
+        return
     await bot.copy_message(message.from_id, message.from_id, message_id, reply_markup=custom_kb)
 
     await message.answer(texts.confirm_message, reply_markup=kb.message_menu_kb)
     await State.confirmation_message.set()
-
-
-
-
-# @dp.message_handler(state=State.confirmation_message)
-# async def send_welcome(message: types.Message, state: FSMContext):
-#     if message.text == texts.yes:
-#         data = await state.get_data()
-#         chat_id = data.get('channel_id')
-#         message_id = data.get('message_id')
-#         await bot.copy_message(chat_id, message.from_id, message_id)
-#     elif message.text == texts.no:
-#         await message.answer(texts.abort)
-#         await message.answer(texts.enter_code)
-#         await State.entering_code.set()
-#     else:
-#         await message.answer(texts.use_kb)
-    
 
